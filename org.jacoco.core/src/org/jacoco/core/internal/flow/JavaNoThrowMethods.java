b7e2d4aeda97ad309862771ddcb19d5a4c9fc304
Fabian Meumertzheim <fabian@meumertzhe.im>
2021-11-16 13:47:05 +0100

    Reformat with spotless

--- org.jacoco.core/src/org/jacoco/core/internal/flow/JavaNoThrowMethods.java
+++ org.jacoco.core/src/org/jacoco/core/internal/flow/JavaNoThrowMethods.java
@@ -20,55 +20,65 @@ import java.util.HashSet;
 import java.util.Set;
 
 public final class JavaNoThrowMethods {
-    public static boolean isTesting = false;
+	public static boolean isTesting = false;
 
-    public static boolean shouldInstrumentMethodInsn(String owner, String name, String descriptor) {
-        if (owner.startsWith("com/code_intelligence/jazzer/") && !isTesting) {
-            return false;
-        }
-        // We only collect no-throw information for the Java standard library.
-        if (!owner.startsWith("java/")) {
-            return true;
-        }
-        return !LIST.contains(String.format("%s#%s#%s", owner, name, descriptor));
-    }
+	public static boolean shouldInstrumentMethodInsn(String owner, String name,
+			String descriptor) {
+		if (owner.startsWith("com/code_intelligence/jazzer/") && !isTesting) {
+			return false;
+		}
+		// We only collect no-throw information for the Java standard library.
+		if (!owner.startsWith("java/")) {
+			return true;
+		}
+		return !LIST
+				.contains(String.format("%s#%s#%s", owner, name, descriptor));
+	}
 
-    private static final String DATA_FILE_NAME = "java_no_throw_methods_list.dat";
-    private static final String DATA_FILE_RESOURCE_PATH = String.format(
-            "%s/%s", JavaNoThrowMethods.class.getPackage().getName().replace('.', '/'), DATA_FILE_NAME);
-    private static final Set<String> LIST = readJavaNoThrowMethods();
+	private static final String DATA_FILE_NAME = "java_no_throw_methods_list.dat";
+	private static final String DATA_FILE_RESOURCE_PATH = String.format("%s/%s",
+			JavaNoThrowMethods.class.getPackage().getName().replace('.', '/'),
+			DATA_FILE_NAME);
+	private static final Set<String> LIST = readJavaNoThrowMethods();
 
-    private static Set<String> readJavaNoThrowMethods() {
-        // If we successfully appended the agent JAR to the bootstrap class loader path in Agent, the
-        // classLoader property of JavaNoThrowMethods returns null and we have to use the system class
-        // loader instead.
-        ClassLoader classLoader = JavaNoThrowMethods.class.getClassLoader();
-        if (classLoader == null) {
-            classLoader = ClassLoader.getSystemClassLoader();
-        }
-        InputStream resourceStream = classLoader.getResourceAsStream(DATA_FILE_RESOURCE_PATH);
-        if (resourceStream == null) {
-            throw new IllegalStateException(String.format(
-                    "No-throw method signatures not found at resource path: %s%n",
-                    DATA_FILE_RESOURCE_PATH));
-        }
-        BufferedReader resourceReader = new BufferedReader(new InputStreamReader(resourceStream));
-        try {
-            Set<String> list = new HashSet<String>();
-            String line;
-            while ((line  = resourceReader.readLine()) != null) {
-                list.add(line);
-            }
-            return list;
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to load no-throw method list");
-        } finally {
-            try {
-                resourceStream.close();
-            } catch (IOException ignored) {}
-            try {
-                resourceReader.close();
-            } catch (IOException ignored) {}
-        }
-    }
+	private static Set<String> readJavaNoThrowMethods() {
+		// If we successfully appended the agent JAR to the bootstrap class
+		// loader path in Agent, the
+		// classLoader property of JavaNoThrowMethods returns null and we have
+		// to use the system class
+		// loader instead.
+		ClassLoader classLoader = JavaNoThrowMethods.class.getClassLoader();
+		if (classLoader == null) {
+			classLoader = ClassLoader.getSystemClassLoader();
+		}
+		InputStream resourceStream = classLoader
+				.getResourceAsStream(DATA_FILE_RESOURCE_PATH);
+		if (resourceStream == null) {
+			throw new IllegalStateException(String.format(
+					"No-throw method signatures not found at resource path: %s%n",
+					DATA_FILE_RESOURCE_PATH));
+		}
+		BufferedReader resourceReader = new BufferedReader(
+				new InputStreamReader(resourceStream));
+		try {
+			Set<String> list = new HashSet<String>();
+			String line;
+			while ((line = resourceReader.readLine()) != null) {
+				list.add(line);
+			}
+			return list;
+		} catch (IOException e) {
+			throw new IllegalStateException(
+					"Failed to load no-throw method list");
+		} finally {
+			try {
+				resourceStream.close();
+			} catch (IOException ignored) {
+			}
+			try {
+				resourceReader.close();
+			} catch (IOException ignored) {
+			}
+		}
+	}
 }
